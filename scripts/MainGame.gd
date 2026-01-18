78d386ff1f8058a4b10aef5d32b647cd6e3522b0
VIPPYJose <josevon.gerald@gmail.com>
2026-01-17 23:57:23 -0800

    push

--- scripts/MainGame.gd
+++ scripts/MainGame.gd
@@ -6,6 +6,13 @@ extends Node2D
 func _ready():
 	spawn_player()
 	spawn_dog()
+	setup_hud()
+
+func setup_hud():
+	var hud_scene = load("res://scenes/bottom_hud.tscn")
+	if hud_scene:
+		var hud = hud_scene.instantiate()
+		add_child(hud)
 
 func spawn_player():
 	var variant = GameState.character_variant
@@ -31,6 +38,10 @@ func spawn_player():
 	name_tag.position = Vector2(-50, -40)
 	name_tag.visible = false
 	name_tag.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+	# Smaller pixel font look
+	name_tag.add_theme_font_size_override("font_size", 10)
+	
 	player.add_child(name_tag)
 	
 	player.mouse_entered.connect(func(): 
@@ -55,6 +66,10 @@ func spawn_dog():
 	name_tag.position = Vector2(-50, -40)
 	name_tag.visible = false
 	name_tag.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+	# Smaller pixel font look
+	name_tag.add_theme_font_size_override("font_size", 10)
+	
 	dog.add_child(name_tag)
 	
 	dog.mouse_entered.connect(func(): 
